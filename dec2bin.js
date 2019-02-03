b5fe903a765c0bc284c96b23f747beef3b61ba88
Oleksandr Prostotin <37674127+prostotin@users.noreply.github.com>
2019-02-02 22:39:36 -0500

    Update dec2bin.js

--- dec2bin.js
+++ dec2bin.js
@@ -1,18 +1,48 @@
-function convertToBinary(){	
-	var decimal = document.getElementById('decimal').value;
-	var output = [];	
-	var sNumber = decimal.toString();		
-		for(var i = 0, len = sNumber.length; i < len; i++){
-		  output.push(+sNumber.charAt(i));
-		}		
-		var binary = [];				
-	do{		
-		var remainder = decimal%2;
-		binary.push(remainder);	
-		decimal = (parseInt(decimal) - remainder)/2;		
-	}while(parseInt(decimal) / 2 != 0);
-		
-	binary.reverse();
-	document.getElementById('result').value = binary.join("");		
-	}
-		
+function convertToBinary() {
+
+  var decimal = document.getElementById('decimal').value;
+  var output = [];
+  var sNumber = decimal.toString();
+  if (sNumber.charAt(0) == "-") {
+    decimal = decimal * -1;
+    for (var i = 1, len = sNumber.length; i < len; i++) {
+      output.push(+sNumber.charAt(i));
+    }
+    var binary = [];
+    do {
+      var remainder = decimal % 2;
+      binary.push(remainder);
+      decimal = (parseInt(decimal) - remainder) / 2;
+    } while (parseInt(decimal) / 2 != 0);
+
+    binary.reverse();
+    for (var i = 0; i < binary.length; i++) {
+
+      if (binary[i] == 1) {
+
+        binary[i] = 0;
+      } else {
+        binary[i] = 1;
+      }
+    }
+
+    document.getElementById('result').value = binary.join("");
+
+  } else {
+    for (var i = 0, len = sNumber.length; i < len; i++) {
+      output.push(+sNumber.charAt(i));
+    }
+    var binary = [];
+    do {
+      var remainder = decimal % 2;
+      binary.push(remainder);
+      decimal = (parseInt(decimal) - remainder) / 2;
+    } while (parseInt(decimal) / 2 != 0);
+
+    binary.reverse();
+    document.getElementById('result').value = binary.join("");
+
+    //document.getElementById('result').value = ((document.getElementById('decimal').value >>> 0).toString(2));	
+  }
+
+}
